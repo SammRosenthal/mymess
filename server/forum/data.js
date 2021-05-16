9f3eea520bac98eb832e3d27da0d496f3f5b3571
Sam Rosenthal <samrrosenthal@gmail.com>
2021-05-16 09:12:42 -0700

    added hack for env variables

--- server/forum/data.js
+++ server/forum/data.js
@@ -1,21 +1,27 @@
 const pgp = require("pg-promise")();
 const { Send } = require("@material-ui/icons");
 const { Client } = require("pg");
-
 const connection = new Client();
-connection.user = process.env.DB_USERNAME;
-console.log(process.env.DB_USERNAME);
-connection.password = process.env.DB_PASSWORD;
-console.log(process.env.DB_PASSWORD);
-connection.database = "postgres";
-connection.host = "localhost";
-connection.port = 5432;
-connection.ssl = false;
 
-const db = pgp(connection);
+let db;
+
+setTimeout(setDBInfo, 0);
+
+function setDBInfo() {
+  connection.user = process.env.DB_USERNAME;
+  console.log(process.env.DB_USERNAME);
+  connection.password = process.env.DB_PASSWORD;
+  console.log(process.env.DB_PASSWORD);
+  connection.database = "postgres";
+  connection.host = "localhost";
+  connection.port = 5432;
+  connection.ssl = false;
+  db = pgp(connection);
+}
 
 module.exports = {
   getAllPosts: () => {
+    console.log(process.env.DB_PASSWORD, process.env.DB_USERNAME);
     db.one("SELECT * FROM forum")
       .then((v) => {
         console.log(v);
