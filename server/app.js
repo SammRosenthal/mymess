142217cd559a7bca9a4415c4a75bd5a3f8d9ed32
Sam Rosenthal <samrrosenthal@gmail.com>
2021-05-16 11:15:19 -0700

    set up prisma and added babel for server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const forumRouter = require("./forum/router");
-require("dotenv").config({ path: __dirname + "\\.env" });
+require("dotenv").config({ path: "./.env" });
 const app = express();
-const PORT = 8080;
 
 app.use("/forum", forumRouter);
 
-app.listen(PORT, () => console.log(`server running on localhost: ${PORT}`));
+app.listen(8080, () => console.log(`server running on localhost: ${8080}`));
--- server/forum/data.js
+++ server/forum/data.js
@@ -1,31 +1,14 @@
-const pgp = require("pg-promise")();
-const { Send } = require("@material-ui/icons");
-const { Client } = require("pg");
-const connection = new Client();
+import { PrismaClient } from "@prisma/client";
 
-let db;
-
-setTimeout(setDBInfo, 0);
-
-function setDBInfo() {
-  connection.user = process.env.DB_USERNAME;
-  console.log(process.env.DB_USERNAME);
-  connection.password = process.env.DB_PASSWORD;
-  console.log(process.env.DB_PASSWORD);
-  connection.database = "postgres";
-  connection.host = "localhost";
-  connection.port = 5432;
-  connection.ssl = false;
-  db = pgp(connection);
-}
+const prisma = new PrismaClient();
 
 module.exports = {
-  getAllPosts: () => {
-    console.log(process.env.DB_PASSWORD, process.env.DB_USERNAME);
-    db.one("SELECT * FROM forum")
-      .then((v) => {
-        console.log(v);
-      })
-      .catch((e) => console.log(e));
-  },
+  getAllPosts: getAllPosts()
+    .catch((e) => console.error("Error gathering all Forum posts", e.message))
+    .finally(async () => await prisma.$disconnect()),
 };
+
+async function getAllPosts() {
+  const allPosts = await prisma.forumPosts.findMany();
+  console.log(allPosts);
+}
