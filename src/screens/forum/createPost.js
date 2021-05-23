b1c824f7856ac79edf932ce91a2e41b24eb37a7e
Sam Rosenthal <samrrosenthal@gmail.com>
2021-05-23 13:03:28 -0700

    added create post form validation

--- src/screens/forum/createPost.js
+++ src/screens/forum/createPost.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Container, Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
@@ -46,12 +46,60 @@ export default function CreatePost(props) {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [body, setBody] = useState("");
-  const [currentUser, setCurrentUser] = useState({});
+  const [validation, setValidation] = useState({
+    title: false,
+    summary: false,
+    body: false,
+  });
 
   function resetFormFields() {
     setTitle("");
     setSummary("");
     setBody("");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (validateForm()) {
+      submitNewPost();
+    }
+  }
+
+  function validateForm() {
+    const newValidation = { ...validation };
+    validateTitle(newValidation);
+    validateSummary(newValidation);
+    validateBody(newValidation);
+
+    setValidation(newValidation);
+    return !Object.values(newValidation).some((v) => v === true);
+  }
+
+  function validateTitle(validationObj) {
+    if (title.length === 0) {
+      validationObj.title = true;
+      return false;
+    }
+    validationObj.title = false;
+    return true;
+  }
+
+  function validateSummary(validationObj) {
+    if (summary.length === 0) {
+      validationObj.summary = true;
+      return false;
+    }
+    validationObj.summary = false;
+    return true;
+  }
+
+  function validateBody(validationObj) {
+    if (body.length === 0) {
+      validationObj.body = true;
+      return false;
+    }
+    validationObj.body = false;
+    return true;
   }
 
   function submitNewPost() {
@@ -68,10 +116,6 @@ export default function CreatePost(props) {
       });
   }
 
-  useEffect(() => {
-    setCurrentUser(props.user);
-  }, [props.user]);
-
   function cancelPostSubmisison() {
     resetFormFields();
     props.history.goBack();
@@ -79,78 +123,83 @@ export default function CreatePost(props) {
 
   return (
     <Container className={styles.root}>
-      <Paper className={styles.paper}>
-        <Grid container spacing={3} className={styles.centerGrid}>
-          <Grid item xs={12}>
-            <Paper className={styles.title} elevation={0}>
-              Create Your Post
-            </Paper>
-          </Grid>
-          <Grid item xs={12} className={styles.singleLineInput}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="Post Title"
-              variant="outlined"
-              onChange={(e) => setTitle(e.target.value)}
-              value={title}
-            />
-          </Grid>
-          <Grid item xs={12} className={styles.singleLineInput}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="Post Summary (Text Preview)"
-              multiline
-              variant="outlined"
-              rows={6}
-              onChange={(e) => setSummary(e.target.value)}
-              value={summary}
-            />
-          </Grid>
-          <Grid item xs={12} className={styles.singleLineInput}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="Post Body"
-              variant="outlined"
-              multiline
-              rows={6}
-              onChange={(e) => setBody(e.target.value)}
-              value={body}
-            />
-          </Grid>
-          <Grid item xs={6} className={styles.buttonContainer}>
-            {props.isAuthenticated ? (
+      <form onSubmit={handleSubmit} noValidate>
+        <Paper className={styles.paper}>
+          <Grid container spacing={3} className={styles.centerGrid}>
+            <Grid item xs={12}>
+              <Paper className={styles.title} elevation={0}>
+                Create Your Post
+              </Paper>
+            </Grid>
+            <Grid item xs={12} className={styles.singleLineInput}>
+              <TextField
+                required={true}
+                fullWidth
+                label="Post Title"
+                variant="outlined"
+                error={validation.title}
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </Grid>
+            <Grid item xs={12} className={styles.singleLineInput}>
+              <TextField
+                required={true}
+                fullWidth
+                label="Post Summary (Text Preview)"
+                multiline
+                variant="outlined"
+                rows={6}
+                error={validation.summary}
+                onChange={(e) => setSummary(e.target.value)}
+                value={summary}
+              />
+            </Grid>
+            <Grid item xs={12} className={styles.singleLineInput}>
+              <TextField
+                required={true}
+                fullWidth
+                label="Post Body"
+                variant="outlined"
+                multiline
+                rows={6}
+                error={validation.body}
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+              />
+            </Grid>
+            <Grid item xs={6} className={styles.buttonContainer}>
+              {props.isAuthenticated ? (
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className={styles.addButton}
+                >
+                  Add
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={styles.addButton}
+                  disabled
+                >
+                  Add
+                </Button>
+              )}
               <Button
                 variant="contained"
-                color="primary"
-                className={styles.addButton}
-                onClick={submitNewPost}
+                color="secondary"
+                className={styles.cancelButton}
+                onClick={cancelPostSubmisison}
               >
-                Add
+                Cancel
               </Button>
-            ) : (
-              <Button
-                variant="contained"
-                color="primary"
-                className={styles.addButton}
-                disabled
-              >
-                Add
-              </Button>
-            )}
-            <Button
-              variant="contained"
-              color="secondary"
-              className={styles.cancelButton}
-              onClick={cancelPostSubmisison}
-            >
-              Cancel
-            </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </form>
     </Container>
   );
 }
