c39416373a552ebd33a6435738ea2832090d6fb0
Sam Rosenthal <samrrosenthal@gmail.com>
2021-05-23 15:48:55 -0700

    formatting and vim stuff

--- server/forum/data.js
+++ server/forum/data.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -8,17 +8,18 @@ const RESPONSE_CODE_500 = 500;
 module.exports = {
   getAllPosts: async () => {
     return getAllPosts()
-      .catch((e) => console.error("Error gathering all Forum posts", e.message))
+      .catch((e) => console.error('Error gathering all Forum posts', e.message))
       .finally(async () => await prisma.$disconnect());
   },
   addPost: async (postContent) => {
     await addNewPost(postContent)
       .catch((e) => {
-        console.error("Error adding new post to Forum DB", e.message);
+        console.error('Error adding new post to Forum DB', e.message);
         return RESPONSE_CODE_500;
       })
       .finally(async () => {
-        await prisma.$disconnect;
+        /* eslint-disable */
+        prisma.$disconnect;
         return RESPONSE_CODE_200;
       });
   },
--- server/forum/logic.js
+++ server/forum/logic.js
@@ -1,13 +1,16 @@
-const { ContactSupportOutlined } = require("@material-ui/icons");
-const forumData = require("./data.js");
+const forumData = require('./data');
 
 module.exports = {
-  getAllPosts: async (res) => {
+  getAllPosts: async () => {
     const allPosts = await forumData.getAllPosts();
     return allPosts;
   },
   addPost: async (postContent) => {
-    // TODO: validate post content here
-    return await forumData.addPost(postContent);
+    validatePostInput(postContent);
+    return forumData.addPost(postContent);
   },
 };
+
+function validatePostInput() {
+
+}
