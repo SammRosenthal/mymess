bbcf31fb3e964f6e9a7cdbf6be7fec0422075312
Sam Rosenthal <samrrosenthal@gmail.com>
2021-05-25 17:46:28 -0700

    formated forum container

---
+++ src/screens/forum/forumContainer.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from 'axios';
+import { Button } from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+import { Link } from 'react-router-dom';
+import ForumPost from './forumPost';
+
+// eslint-disable-next-line
+const BootstrapButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#0063cc',
+    borderColor: '#0063cc',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    width: '100%',
+  },
+});
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: 75,
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    minWidth: '86.6%',
+    margin: theme.spacing(1),
+    textDecoration: 'none',
+    color: 'white',
+  },
+});
+
+export default function ForumContainer(props) {
+  const { isAuthenticated } = props;
+  const { button, root } = useStyles();
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/forum').then((v) => {
+      setAllPosts(v.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Container variant="contained" className={root}>
+        <ThemeProvider theme={theme}>
+          <Link
+            className={button}
+            to={isAuthenticated ? '/forum/createPost' : ''}
+            disabled={!isAuthenticated}
+          >
+            <Button variant="contained" color="primary" className={button}>
+              Add Post
+            </Button>
+          </Link>
+        </ThemeProvider>
+        {allPosts ? (
+          allPosts.map((post) => (
+            <ForumPost
+              key={post.id}
+              postId={post.id}
+              authorId={post.authorId}
+              body={post.body}
+              createdAt={post.createdAt}
+              updatedAt={post.updatedAt}
+              summary={post.summary}
+              title={post.title}
+              formattedDate={post.formattedDate}
+            />
+          ))
+        ) : (
+          <h1>loading</h1>
+        )}
+      </Container>
+    </>
+  );
+}
+
+ForumContainer.defaultProps = {
+  isAuthenticated: false,
+};
+
+ForumContainer.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
